ca8bd67fc29123b082a7396308731388f0881009
SynapticSugar <april.deirdre@gmail.com>
2018-07-23 22:23:02 -0400

    Added VGG16 detector.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -21,6 +21,7 @@ from mobiledet.models.keras_yolo import yolo_eval, decode_yolo_output, create_mo
 from keras import backend as K
 import time
 import tensorflow
+from keras.models import load_model
 
 STATE_COUNT_THRESHOLD = 3
 
@@ -53,7 +54,7 @@ class TLDetector(object):
         self.is_site = self.config['is_site']
 
         #TODO Remove hack to force site mode or ground_truth for testing
-        self.is_site = True
+        #self.is_site = True
         self.ground_truth = False
 
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
@@ -70,51 +71,64 @@ class TLDetector(object):
         self.waypoints_2d = None
         self.waypoint_tree = None
 
-        # Detector Stuff
-        self.model_image_size = None
-        self.sess = None
-        self.initialized = False
-        model_path = os.path.expanduser('./weights/mobilenet_s2_best.FalseFalse.h5')
-        anchors_path = os.path.expanduser('./model_data/lisa_anchors.txt')
-        classes_path = os.path.expanduser('./model_data/lisa_classes.txt')
+
+        if self.is_site:
+            # Detector Stuff
+            self.model_image_size = None
+            self.sess = None
+            self.initialized = False
+            model_path = os.path.expanduser('./weights/mobilenet_s2_best.FalseFalse.h5')
+            anchors_path = os.path.expanduser('./model_data/lisa_anchors.txt')
+            classes_path = os.path.expanduser('./model_data/lisa_classes.txt')
+            
+            self.class_names  = utils.get_classes(classes_path)
+            anchors = utils.get_anchors(anchors_path)
+            if SHALLOW_DETECTOR:
+                anchors = anchors * 2
+            
+            print(self.class_names)
+            print(anchors)
+            
+            self.yolo_model, yolo_model_for_training = create_model(anchors, self.class_names, load_pretrained=True, 
+            feature_extractor=FEATURE_EXTRACTOR, pretrained_path=model_path, freeze_body=True)
+
+            model_file_basename, file_extension = os.path.splitext(os.path.basename(model_path))
+
+            model_input = self.yolo_model.input.name.replace(':0', '') # input_1
+            model_output = self.yolo_model.output.name.replace(':0', '') # conv2d_3/BiasAdd
+
+            sess = K.get_session()
+            width, height, channels = int(self.yolo_model.input.shape[2]), int(self.yolo_model.input.shape[1]), int(self.yolo_model.input.shape[3])
+
+            # END OF keras specific code
+
+            # Check if model is fully convolutional, assuming channel last order.
+            self.model_image_size = self.yolo_model.layers[0].input_shape[1:3]
+
+            self.sess = K.get_session()  # TODO: Remove dependence on Tensorflow session.
+
+            # Generate output tensor targets for filtered bounding boxes.
+            self.yolo_outputs = decode_yolo_output(self.yolo_model.output, anchors, len(self.class_names))
+
+            self.input_image_shape = K.placeholder(shape=(2, ))
+            self.boxes, self.scores, self.classes = yolo_eval(
+                self.yolo_outputs,
+                self.input_image_shape,
+                score_threshold=.6,
+                iou_threshold=.6)
+
+            self.graph = tensorflow.get_default_graph()
+
         
-        self.class_names  = utils.get_classes(classes_path)
-        anchors = utils.get_anchors(anchors_path)
-        if SHALLOW_DETECTOR:
-            anchors = anchors * 2
-        
-        print(self.class_names)
-        print(anchors)
-        
-        self.yolo_model, yolo_model_for_training = create_model(anchors, self.class_names, load_pretrained=True, 
-        feature_extractor=FEATURE_EXTRACTOR, pretrained_path=model_path, freeze_body=True)
+        else:
+            try:
+                model_path = os.path.expanduser('./weights/vgg16_1.h5')
+                self.vgg_model = load_model(model_path)
+                self.graph = tensorflow.get_default_graph()
+            except:
+                rospy.logerr(
+                    "Could not load model. Have you downloaded the vgg16_1.h5 file to the weights folder? You can download it here: hhtps://s3-eu-west-1.amazonaws.com/sdcnddata/vgg_16_1.h5")
 
-        model_file_basename, file_extension = os.path.splitext(os.path.basename(model_path))
-
-        model_input = self.yolo_model.input.name.replace(':0', '') # input_1
-        model_output = self.yolo_model.output.name.replace(':0', '') # conv2d_3/BiasAdd
-
-        sess = K.get_session()
-        width, height, channels = int(self.yolo_model.input.shape[2]), int(self.yolo_model.input.shape[1]), int(self.yolo_model.input.shape[3])
-
-        # END OF keras specific code
-
-        # Check if model is fully convolutional, assuming channel last order.
-        self.model_image_size = self.yolo_model.layers[0].input_shape[1:3]
-
-        self.sess = K.get_session()  # TODO: Remove dependence on Tensorflow session.
-
-        # Generate output tensor targets for filtered bounding boxes.
-        self.yolo_outputs = decode_yolo_output(self.yolo_model.output, anchors, len(self.class_names))
-
-        self.input_image_shape = K.placeholder(shape=(2, ))
-        self.boxes, self.scores, self.classes = yolo_eval(
-            self.yolo_outputs,
-            self.input_image_shape,
-            score_threshold=.6,
-            iou_threshold=.6)
-
-        self.graph = tensorflow.get_default_graph()
 
         self.initialized = True
 
@@ -197,7 +211,8 @@ class TLDetector(object):
 
     def classify_traffic_light(self):
         """Determine the state of the traffic light in the scene (if any)
-           Using a VGG16 to classify.
+           Using a VGG16 network to classify.
+           https://github.com/FabianHertwig/CarND-Capstone
 
         Args:
             None
@@ -207,12 +222,27 @@ class TLDetector(object):
                  UNKNOWN if not found
 
         """
-        # TODO return the actual detected state
+        if self.vgg_model and self.initialized:
+            cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
+            with self.graph.as_default():
+                x = cv2.resize(src=cv_image, dsize=(256, 256))
+                x = x / 255.0
+                x = np.expand_dims(x, 0)
+                predicted_class = self.vgg_model.predict(x)
+                predicted_class = np.argmax(predicted_class)
+                if predicted_class == 0:
+                    return TrafficLight.RED
+                elif predicted_class == 1:
+                    return TrafficLight.YELLOW
+                elif predicted_class == 2:
+                    return TrafficLight.GREEN
+
         return TrafficLight.UNKNOWN
 
     def detect_traffic_light(self):
         """Determine the state of the traffic light in the scene (if any)
            Using a Yolo_V2 network to detect and classify in a single step.
+           https://github.com/darknight1900/MobileDet
 
         Args:
             None
@@ -228,12 +258,15 @@ class TLDetector(object):
         # warning=2   YELLOW=1
         # dontcare=3  UNKNOWN=4
 
-        getstate = dict(stop= 0, warning = 1, go = 2, dontcare = 4)
+        getstate = dict(stop =     TrafficLight.RED,
+                        warning =  TrafficLight.YELLOW, 
+                        go =       TrafficLight.GREEN, 
+                        dontcare = TrafficLight.UNKNOWN)
 
         predicted_class = 'dontcare'
 
         if self.sess and self.initialized:
-            cv_image = self.bridge.imgmsg_to_cv2(self.camera_image)
+            cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
             # resized_image = cv_image.resize(
             #     tuple(reversed(self.model_image_size)))
             height, width, channels = cv_image.shape
